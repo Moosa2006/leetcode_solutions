69690595b114fc0841a176ee24d64ae07b087485
Moosa <moosarajput2006@gmail.com>
2026-07-29 15:41:02 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,8 +3,15 @@ public:
     bool isAnagram(string s, string t) {
         int n = s.length();
         if(n != t.length()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+
+        vector<int> count(26,0);
+        for(int i=0;i<n;i++){
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
     }
 };
