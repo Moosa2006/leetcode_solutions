9177e9a16d030692d8d7638c9dd0bde6c644db10
Moosa <moosarajput2006@gmail.com>
2026-08-06 12:06:27 +0530

    Time: 4 ms (67.53%), Space: 13.2 MB (57.71%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,86 +1,19 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int number = 0;
-        if(s[0] == 'I'){
-                number = number+1;
-            }
-            else if(s[0]=='V'){
+        int res = 0;
+        unordered_map<char,int> roman{
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+        };
+        for(int i=0;i<s.size()-1;i++){
+            if(roman[s[i]] < roman[s[i+1]]){
+                res -= roman[s[i]];
                 
-                
-                    number = number +5;}
-            else if(s[0]=='X'){
-                
-                    number = number +10;
             }
-            else if(s[0]=='L'){
-                
-                    number = number +50;
-            }
-            else if(s[0]=='C'){
-                     
-                    number = number +100;
-            }
-            else if(s[0]=='D'){
-                
-                
-                    number = number +500;
-            }
-            else if(s[0]=='M'){
-                
-                    number = number +1000;}
-           
-
-
-        for(int i=1;i<s.size();i++){
-
-            if(s[i] == 'I'){
-                number = number+1;
-            }
-            else if(s[i]=='V'){
-                if(s[i-1] == 'I'){
-                    number = number+3;
-                }
-                else{
-                    number = number +5;}
-            }
-            else if(s[i]=='X'){
-                if(s[i-1] == 'I'){
-                    number = number+8;
-                }
-                else{
-                    number = number +10;}
-            }
-            else if(s[i]=='L'){
-                if(s[i-1] == 'X'){
-                    number = number+30;
-                }
-                else{
-                    number = number +50;}
-            }
-            else if(s[i]=='C'){
-                                if(s[i-1] == 'X'){
-                    number = number+80;
-                }
-                else{
-                    number = number +100;}
-            }
-            else if(s[i]=='D'){
-                if(s[i-1] == 'C'){
-                    number = number+300;
-                }
-                else{
-                    number = number +500;}
-            }
-            else if(s[i]=='M'){
-                if(s[i-1] == 'C'){
-                    number = number+800;
-                }
-                else{
-                    number = number +1000;}
+            else{
+                res += roman[s[i]];
             }
         }
-        return number;
-        
+        return res+roman[s.back()];
     }
 };
